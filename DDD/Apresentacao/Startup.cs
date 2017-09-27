2c084217f16400c09c0caa24c87a9721b52cda1b
cmunhozprog <cmunhozprog02@hotmail.com>
2017-09-27 00:01:35 -0300

    Criação do Projeto Apresentação

---
+++ DDD/Apresentacao/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Apresentacao.Startup))]
+namespace Apresentacao
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
