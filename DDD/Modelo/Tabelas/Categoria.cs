45e61b4e6d6a9527619842a22228da3c30b80a00
cmunhozprog02Hotmail <cmunhozprog02@hotmail.com>
2017-09-28 19:12:40 -0300

    Solucionado o problema com o Migration

--- DDD/Modelo/Tabelas/Categoria.cs
+++ DDD/Modelo/Tabelas/Categoria.cs
@@ -1,15 +1,23 @@
 ﻿using Modelo.Cadastros;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Modelo.Tabelas
 {
     public class Categoria
     {
+        
         public long? CategoriaId { get; set; }
+
+        //[Required(ErrorMessage = "O este campo é requerido")]
 
         public string Nome { get; set; }
 
-        public string Observacao { get; set; }
+        //public string Observacao { get; set; }
+
+        //public bool teste { get; set; }
+
+        public string Teste02 { get; set; }
 
         public virtual ICollection<Produto> Produto { get; set; }
     }
--- DDD/Presistencia/Migrations/Configuration.cs
+++ DDD/Presistencia/Migrations/Configuration.cs
@@ -10,6 +10,7 @@ namespace Presistencia.Migrations
         public Configuration()
         {
             AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;
         }
 
         protected override void Seed(Presistencia.Contexts.EFContext context)
