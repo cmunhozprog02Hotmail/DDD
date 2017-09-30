a412fe56ff12ced22627b977132f9b16f93766e1
cmunhozprog02Hotmail <cmunhozprog02@hotmail.com>
2017-09-30 15:21:03 -0300

    Projeto03

---
+++ DDD/Apresentacao/Controllers/Produto3Controller.cs
@@ -0,0 +1,153 @@
+﻿using System.Net;
+using System.Web.Mvc;
+using Modelo.Cadastros;
+using Servico.Cadastros;
+
+namespace Apresentacao.Controllers
+{
+    public class Produto3Controller : Controller
+    {
+        private ProdutoServico produtoServico = new ProdutoServico();
+        private CategoriaServico categoriaServico = new CategoriaServico();
+        private FabricanteServico fabricanteServico = new FabricanteServico();
+
+        // Para delete edit e details
+        private ActionResult ObterVisaoProdutoPorId(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Produto produto = produtoServico.ObterProdutoPorId((long)id);
+            if (produto == null)
+            {
+                return HttpNotFound();
+            }
+            return View(produto);
+        }
+
+        // Create e Edit SelectList
+        private void PopularViewBag(Produto produto = null)
+        {
+            if (produto == null)
+            {
+                ViewBag.CategoriaId = new SelectList(categoriaServico.ObterCategoriasClassificadasPorNome(), "CategoriaId", "Nome");
+                ViewBag.FabricanteId = new SelectList(fabricanteServico.ObterFabricantesClassificadosPorNome(), "FabricanteId", "Nome");
+            }
+            else
+            {
+                ViewBag.CategoriaId = new SelectList(categoriaServico.ObterCategoriasClassificadasPorNome(),
+                    "CategoriaId", "Nome", produto.CategoriaId);
+
+                ViewBag.FabricanteId = new SelectList(fabricanteServico.ObterFabricantesClassificadosPorNome(),
+                   "FabricanteId", "Nome", produto.FabricanteId);
+            }
+        }
+
+        // HTTP Salvar
+        private ActionResult GravarProduto(Produto produto)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    produtoServico.GravarProduto(produto);
+                    return RedirectToAction("Index");
+                }
+                PopularViewBag(produto);
+                return View(produto);
+            }
+            catch
+            {
+                return View(produto);
+            }
+        }
+
+
+        // GET: Produtoes
+        public ActionResult Index()
+        {
+
+            return View(produtoServico.ObterProdutosClassificadosPorNome());
+        }
+
+        // GET: Produtoes/Details/5
+        public ActionResult Details(long? id)
+        {
+            return ObterVisaoProdutoPorId(id);
+        }
+
+        // GET: Produtoes/Create
+        public ActionResult Create()
+        {
+            PopularViewBag();
+            return View();
+        }
+
+        // POST: Produtoes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ProdutoId,Nome,Descricao")] Produto produto)
+        {
+
+            return GravarProduto(produto);
+
+
+
+        }
+
+        // GET: Produtoes/Edit/5
+        public ActionResult Edit(long? id)
+        {
+            PopularViewBag(produtoServico.ObterProdutoPorId((long)id));
+            return ObterVisaoProdutoPorId(id);
+        }
+
+        // POST: Produtoes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ProdutoId,Nome,Descricao")] Produto produto)
+        {
+            return GravarProduto(produto);
+        }
+
+        // GET: Produtoes/Delete/5
+        public ActionResult Delete(long? id)
+        {
+            return ObterVisaoProdutoPorId(id);
+        }
+
+        // POST: Produtoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(long id)
+        {
+            try
+            {
+                Produto produto = produtoServico.EliminarProdutoPorId(id);
+                TempData["Message"] = "Produto " + produto.Nome.ToUpper() + " foi removido";
+                return RedirectToAction("Index");
+
+            }
+            catch (System.Exception)
+            {
+
+                throw;
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                //db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- DDD/Modelo/Cadastros/Produto.cs
+++ DDD/Modelo/Cadastros/Produto.cs
@@ -1,6 +1,7 @@
 ﻿using Modelo.Tabelas;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,14 +10,21 @@ namespace Modelo.Cadastros
 {
     public class Produto
     {
+        [Display (Name = "id")]
         public long? ProdutoId { get; set; }
 
+        [StringLength(200, ErrorMessage = "O nome do produto precisa ter no mínimo 10 caracteres")]
+        [Required(ErrorMessage = "Informe o nome do produto")]
         public string Nome { get; set; }
 
-        public string Descricao { get; set; }
+        [Display(Name = "Data de Cadastro")]
+        //[Required(ErrorMessage = "Informe a data de cadastro do produto")]
+        public DateTime? DataCadastro { get; set; }
 
+        [Display(Name = "Categoria")]
         public long? CategoriaId { get; set; }
 
+        [Display(Name = "Fabricante")]
         public long? FabricanteId { get; set; }
 
         public virtual Fabricante Fabricante { get; set; }
